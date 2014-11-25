spinnaker/orca

get app config from mayo and use health check names to filter providers

orca-core/src/main/groovy/com/netflix/spinnaker/orca/pipeline/Pipeline.groovy
@@ -32,6 +32,8 @@ class Pipeline implements Serializable {
   final Map<String, Serializable> trigger = [:]
   private final List<PipelineStage> stages = []
 
+  final Map<String, Serializable> config = [:]
+
   ImmutableMap<String, Serializable> getTrigger() {
     ImmutableMap.copyOf(trigger)
   }

plumpy/spinjava

orca/orca-core/src/main/groovy/com/netflix/spinnaker/orca/pipeline/model/Pipeline.groovy
@@ -30,6 +30,8 @@ class Pipeline extends Execution<Pipeline> {
   String name
   final Map<String, Object> trigger = [:]
 
+  final Map<String, Serializable> appConfig = [:]
+
   @JsonIgnore
   ExecutionStatus getStatus() {
     def status = stages.status.reverse().find {