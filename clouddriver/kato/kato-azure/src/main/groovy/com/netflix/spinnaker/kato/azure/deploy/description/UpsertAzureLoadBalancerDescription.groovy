tomzhang/clouddriver-msft

Add Azure UpsertLoadBalancer support

kato/kato-azure/src/main/groovy/com/netflix/spinnaker/kato/azure/deploy/description/UpsertAzureLoadBalancerDescription.groovy
@@ -1,0 +1,79 @@
+/*
+ * Copyright 2015 The original authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.kato.azure.deploy.description
+
+import com.netflix.spinnaker.clouddriver.azure.security.AzureCredentials
+
+class UpsertAzureLoadBalancerDescription {
+  String cloudProvider
+  String providerType
+  String appName
+  String loadBalancerName
+  String stack
+  String detail
+  AzureCredentials credentials
+  String region
+  String vnet
+  List<AzureLoadBalancerProbes> probes
+  String securityGroups
+  List<AzureLoadBalancingRules> loadBalancingRules
+  List<AzureLoadBalancerInboundNATRules> inboundNATRules
+  String name
+  String user
+
+
+  static class AzureLoadBalancerProbes {
+    enum AzureLoadBalancerProbesType {
+      HTTP, TCP
+    }
+
+    String probeName
+    AzureLoadBalancerProbesType probeProtocol
+    Integer probePort
+    String probePath
+    Integer probeInterval
+    Integer unhealthyThreshold
+  }
+
+  static class AzureLoadBalancingRules {
+    enum AzureLoadBalancingRulesType {
+      TCP, UDP
+    }
+
+    String ruleName
+    AzureLoadBalancingRulesType protocol
+    Integer externalPort
+    Integer backendPort
+    String probeName
+    String persistence
+    Integer idleTimeout
+  }
+
+  static class AzureLoadBalancerInboundNATRules {
+    enum AzureLoadBalancerInboundNATRulesProtocolType {
+      HTTP, TCP
+    }
+    enum AzureLoadBalancerInboundNATRulesServiceType {
+      SSH
+    }
+
+    String ruleName
+    AzureLoadBalancerInboundNATRulesServiceType serviceType
+    AzureLoadBalancerInboundNATRulesProtocolType protocol
+    Integer port
+  }
+}

kato/kato-azure/src/main/groovy/com/netflix/spinnaker/kato/azure/deploy/validators/UpsertAzureLoadBalancerDescriptionValidator.groovy
@@ -1,0 +1,50 @@
+/*
+ * Copyright 2015 The original authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.kato.azure.deploy.validators
+
+import com.netflix.spinnaker.clouddriver.security.AccountCredentialsProvider
+import com.netflix.spinnaker.kato.deploy.DescriptionValidator
+import com.netflix.spinnaker.kato.azure.deploy.description.UpsertAzureLoadBalancerDescription
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+import org.springframework.validation.Errors
+
+@Component("upsertAzureLoadBalancerDescriptionValidator")
+class UpsertAzureLoadBalancerDescriptionValidator extends
+  DescriptionValidator<UpsertAzureLoadBalancerDescription> {
+  private static final List<String> SUPPORTED_IP_PROTOCOLS = ["TCP", "UDP"]
+
+  @Autowired
+  AccountCredentialsProvider accountCredentialsProvider
+
+  @Override
+  void validate(List priorDescriptions, UpsertAzureLoadBalancerDescription description, Errors errors) {
+    def helper = new StandardAzureAttributeValidator("upsertAzureLoadBalancerDescription", errors)
+
+    /*
+    helper.validateCredentials(description.accountName, accountCredentialsProvider)
+    helper.validateRegion(description.region)
+    helper.validateName(description.loadBalancerName, "loadBalancerName")
+
+    // If the IP protocol is specified, it must be contained in the list of supported protocols.
+    if (description.ipProtocol && !SUPPORTED_IP_PROTOCOLS.contains(description.ipProtocol)) {
+      errors.rejectValue("ipProtocol",
+        "upsertAzureLoadBalancerDescription.ipProtocol.notSupported")
+    }
+    */
+  }
+}

plumpy/spinjava

clouddriver/kato/kato-azure/src/main/groovy/com/netflix/spinnaker/kato/azure/deploy/description/UpsertAzureLoadBalancerDescription.groovy
@@ -1,0 +1,79 @@
+/*
+ * Copyright 2015 The original authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.kato.azure.deploy.description
+
+import com.netflix.spinnaker.clouddriver.azure.security.AzureCredentials
+
+class UpsertAzureLoadBalancerDescription {
+  String cloudProvider
+  String providerType
+  String appName
+  String loadBalancerName
+  String stack
+  String detail
+  AzureCredentials credentials
+  String region
+  String vnet
+  List<AzureLoadBalancerProbe> probes
+  String securityGroups
+  List<AzureLoadBalancingRule> loadBalancingRules
+  List<AzureLoadBalancerInboundNATRule> inboundNATRules
+  String name
+  String user
+
+
+  static class AzureLoadBalancerProbe {
+    enum AzureLoadBalancerProbesType {
+      HTTP, TCP
+    }
+
+    String probeName
+    AzureLoadBalancerProbesType probeProtocol
+    Integer probePort
+    String probePath
+    Integer probeInterval
+    Integer unhealthyThreshold
+  }
+
+  static class AzureLoadBalancingRule {
+    enum AzureLoadBalancingRulesType {
+      TCP, UDP
+    }
+
+    String ruleName
+    AzureLoadBalancingRulesType protocol
+    Integer externalPort
+    Integer backendPort
+    String probeName
+    String persistence
+    Integer idleTimeout
+  }
+
+  static class AzureLoadBalancerInboundNATRule {
+    enum AzureLoadBalancerInboundNATRulesProtocolType {
+      HTTP, TCP
+    }
+    enum AzureLoadBalancerInboundNATRulesServiceType {
+      SSH
+    }
+
+    String ruleName
+    AzureLoadBalancerInboundNATRulesServiceType serviceType
+    AzureLoadBalancerInboundNATRulesProtocolType protocol
+    Integer port
+  }
+}

clouddriver/kato/kato-azure/src/main/groovy/com/netflix/spinnaker/kato/azure/deploy/validators/UpsertAzureLoadBalancerDescriptionValidator.groovy
@@ -1,0 +1,52 @@
+/*
+ * Copyright 2015 The original authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.kato.azure.deploy.validators
+
+import com.netflix.spinnaker.clouddriver.security.AccountCredentialsProvider
+import com.netflix.spinnaker.kato.deploy.DescriptionValidator
+import com.netflix.spinnaker.kato.azure.deploy.description.UpsertAzureLoadBalancerDescription
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+import org.springframework.validation.Errors
+
+@Component("upsertAzureLoadBalancerDescriptionValidator")
+class UpsertAzureLoadBalancerDescriptionValidator extends
+  DescriptionValidator<UpsertAzureLoadBalancerDescription> {
+  private static final List<String> SUPPORTED_IP_PROTOCOLS = ["TCP", "UDP"]
+
+  @Autowired
+  AccountCredentialsProvider accountCredentialsProvider
+
+  @Override
+  void validate(List priorDescriptions, UpsertAzureLoadBalancerDescription description, Errors errors) {
+    def helper = new StandardAzureAttributeValidator("upsertAzureLoadBalancerDescription", errors)
+
+
+    helper.validateCredentials(description.credentials, accountCredentialsProvider)
+    helper.validateRegion(description.region)
+    helper.validateName(description.loadBalancerName, "loadBalancerName")
+
+    /*
+
+    // If the IP protocol is specified, it must be contained in the list of supported protocols.
+    if (description.ipProtocol && !SUPPORTED_IP_PROTOCOLS.contains(description.ipProtocol)) {
+      errors.rejectValue("ipProtocol",
+        "upsertAzureLoadBalancerDescription.ipProtocol.notSupported")
+    }
+    */
+  }
+}