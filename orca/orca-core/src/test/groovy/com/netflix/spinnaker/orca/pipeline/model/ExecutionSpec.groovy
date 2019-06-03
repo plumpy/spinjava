spinnaker/spinnaker

commit a1de32d0345446e240885b6a792a60b4ff13c2eb

chore(dependencies): Boot 2 and the BOM

orca-core/orca-core.gradle
@@ -24,40 +24,41 @@ test {
 }
 
 dependencies {
-  compile project(":orca-extensionpoint")
-  compile spinnaker.dependency('guava')
-  compile spinnaker.dependency('slf4jApi')
-  testRuntime spinnaker.dependency('slf4jSimple')
+  api("de.huxhorn.sulky:de.huxhorn.sulky.ulid")
+  api("com.netflix.spinnaker.kork:kork-artifacts")
+  api("com.netflix.spinnaker.kork:kork-security")
+  api("com.netflix.spinnaker.kork:kork-exceptions")
+  api("io.reactivex:rxjava")
 
-  spinnaker.group('jackson')
+  implementation(project(":orca-extensionpoint"))
+  implementation("com.google.guava:guava")
+  implementation("org.slf4j:slf4j-api")
+  implementation("com.fasterxml.jackson.core:jackson-annotations")
+  implementation("com.fasterxml.jackson.core:jackson-core")
+  implementation("com.fasterxml.jackson.core:jackson-databind")
+  implementation("com.netflix.eureka:eureka-client")
+  implementation("cglib:cglib-nodep")
+  implementation("com.fasterxml.jackson.datatype:jackson-datatype-guava")
+  implementation("com.netflix.spectator:spectator-api")
+  implementation("com.netflix.spinnaker.kork:kork-core")
+  implementation("net.logstash.logback:logstash-logback-encoder")
+  implementation("org.springframework.security:spring-security-web")
+  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jdk8")
+  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+  implementation("org.apache.commons:commons-lang3")
+  implementation("javax.servlet:javax.servlet-api:4.0.1")
+  implementation("com.jayway.jsonpath:json-path:2.2.0")
 
-  compile spinnaker.dependency('eurekaClient')
-  compile spinnaker.dependency('cglib')
-  compile spinnaker.dependency('jacksonGuava')
-  compile spinnaker.dependency('rxJava')
-  compile spinnaker.dependency('korkSecurity')
-  compile spinnaker.dependency('spectatorApi')
-  compile spinnaker.dependency('kork')
-  compile spinnaker.dependency('korkExceptions')
-  compile spinnaker.dependency('korkArtifacts')
-  compile spinnaker.dependency("logstashEncoder")
-  compile spinnaker.dependency("springSecurityWeb")
-  compile "com.fasterxml.jackson.datatype:jackson-datatype-jdk8:${spinnaker.version('jackson')}"
-  compile "com.fasterxml.jackson.datatype:jackson-datatype-jsr310:${spinnaker.version('jackson')}"
-  compile "com.fasterxml.jackson.module:jackson-module-kotlin:${spinnaker.version('jackson')}"
-  compile "org.apache.commons:commons-lang3:3.7"
-  compile "de.huxhorn.sulky:de.huxhorn.sulky.ulid:8.1.1"
-  compile "javax.servlet:javax.servlet-api:4.0.1"
-  compile('com.jayway.jsonpath:json-path:2.2.0')
+  compileOnly("org.projectlombok:lombok")
+  annotationProcessor("org.projectlombok:lombok")
 
-  compileOnly spinnaker.dependency("lombok")
-  annotationProcessor spinnaker.dependency("lombok")
+  testImplementation(project(":orca-test"))
+  testImplementation(project(":orca-test-groovy"))
+  testImplementation("org.junit.jupiter:junit-jupiter-api")
+  testImplementation("org.assertj:assertj-core")
 
-  testCompile project(":orca-test")
-  testCompile project(":orca-test-groovy")
-  testCompile spinnaker.dependency("junitJupiterApi")
-  testCompile spinnaker.dependency("assertj")
-
-  testRuntime spinnaker.dependency("junitJupiterEngine")
-  testRuntime "org.junit.vintage:junit-vintage-engine:${spinnaker.version('jupiter')}"
+  testRuntimeOnly("org.slf4j:slf4j-simple")
+  testRuntimeOnly("org.junit.jupiter:junit-jupiter-api")
+  testRuntimeOnly "org.junit.vintage:junit-vintage-engine"
 }

orca-core/src/test/groovy/com/netflix/spinnaker/orca/pipeline/model/ExecutionSpec.groovy
@@ -17,7 +17,7 @@
 package com.netflix.spinnaker.orca.pipeline.model
 
 import com.netflix.spinnaker.security.AuthenticatedRequest
-import org.apache.log4j.MDC
+import org.slf4j.MDC
 import spock.lang.Specification
 import static com.netflix.spinnaker.orca.test.model.ExecutionBuilder.pipeline
 import static com.netflix.spinnaker.orca.test.model.ExecutionBuilder.stage

plumpy/spinjava

commit b552ac16aef220a5e001aa098fe8b6c798c577f0

fix issue 4469: orca-core skipping most tests in build (#2957)
* fix(tests): rename fields in LockContextSpec

works around the java.lang.VerifyError mentioned in
https://github.com/spinnaker/spinnaker/issues/4469

* fix(ExecutionSpec): fix test failure due to slf4j-simple

With LockContextSpec fixed in the previous commit to no longer break the
junit-vintage test runner, ExecutionSpec was also broken in commit
a1de32d0345446e240885b6a792a60b4ff13c2eb due to the change from using log4j's
MDC to slf4j's MDC class.

orca-core was setting `slf4j-simple` as `testRuntimeScope` - and
`slf4j-simple` has a no-op MDC implementation which quietly discards any
values set via `MDC.put()`.

To fix this, allow the MDCAdapter provided by `slf4j-api` to be used
instead by removing the dependency on slf4j-simple, and add an assertion
to the test class to ensure that any MDCAdapter instance besides the
no-op adapter is in use.

orca/orca-core/orca-core.gradle
@@ -58,7 +58,6 @@ dependencies {
   testImplementation("org.junit.jupiter:junit-jupiter-api")
   testImplementation("org.assertj:assertj-core")
 
-  testRuntimeOnly("org.slf4j:slf4j-simple")
   testRuntimeOnly("org.junit.jupiter:junit-jupiter-api")
   testRuntimeOnly "org.junit.vintage:junit-vintage-engine"
 }

orca/orca-core/src/test/groovy/com/netflix/spinnaker/orca/locks/LockContextSpec.groovy
@@ -10,14 +10,14 @@ class LockContextSpec extends Specification {
 
   def "builder uses explicitly provided id in when present"() {
     given:
-    def builder = new LockContext.LockContextBuilder.LockValueBuilder(application, type, explicitId, stage)
+    def builder = new LockContext.LockContextBuilder.LockValueBuilder(application, lockType, explicitId, stage)
 
     expect:
     builder.build() == expected
 
     where:
     application = 'app'
-    type = 'pipeline'
+    lockType = 'pipeline'
     explicitId = 'bacon'
 
     stage = ExecutionBuilder.stage {
@@ -25,19 +25,19 @@ class LockContextSpec extends Specification {
     }
 
     expectedId = explicitId
-    expected = new LockManager.LockValue(application, type, expectedId)
+    expected = new LockManager.LockValue(application, lockType, expectedId)
   }
 
   def "builder uses execution id in simple execution"() {
     given:
-    def builder = new LockContext.LockContextBuilder.LockValueBuilder(application, type, explicitId, stage)
+    def builder = new LockContext.LockContextBuilder.LockValueBuilder(application, lockType, explicitId, stage)
 
     expect:
     builder.build() == expected
 
     where:
     application = 'app'
-    type = 'pipeline'
+    lockType = 'pipeline'
     explicitId = null
 
     stage = ExecutionBuilder.stage {
@@ -45,19 +45,19 @@ class LockContextSpec extends Specification {
     }
 
     expectedId = stage.execution.id
-    expected = new LockManager.LockValue(application, type, expectedId)
+    expected = new LockManager.LockValue(application, lockType, expectedId)
   }
 
   def "builder traverses up the hierarchy when execution is triggered by a PipelineTrigger"() {
     given:
-    def builder = new LockContext.LockContextBuilder.LockValueBuilder(application, type, explicitId, stage)
+    def builder = new LockContext.LockContextBuilder.LockValueBuilder(application, lockType, explicitId, stage)
 
     expect:
     builder.build() == expected
 
     where:
     application = 'app'
-    type = 'pipeline'
+    lockType = 'pipeline'
     explicitId = null
 
     parentStage = ExecutionBuilder.stage {
@@ -75,20 +75,20 @@ class LockContextSpec extends Specification {
     stage = exec.stages[0]
 
     expectedId = parentStage.execution.id
-    expected = new LockManager.LockValue(application, type, expectedId)
+    expected = new LockManager.LockValue(application, lockType, expectedId)
 
   }
 
   def "builder traverses up the hierarchy multiple levels when execution is triggered by a PipelineTrigger"() {
     given:
-    def builder = new LockContext.LockContextBuilder.LockValueBuilder(application, type, explicitId, stage)
+    def builder = new LockContext.LockContextBuilder.LockValueBuilder(application, lockType, explicitId, stage)
 
     expect:
     builder.build() == expected
 
     where:
     application = 'app'
-    type = 'pipeline'
+    lockType = 'pipeline'
     explicitId = null
 
 
@@ -103,7 +103,7 @@ class LockContextSpec extends Specification {
     stage = exec.stages[0]
 
     expectedId = grandParentStage.execution.id
-    expected = new LockManager.LockValue(application, type, expectedId)
+    expected = new LockManager.LockValue(application, lockType, expectedId)
 
   }
 

orca/orca-core/src/test/groovy/com/netflix/spinnaker/orca/pipeline/model/ExecutionSpec.groovy
@@ -18,11 +18,19 @@ package com.netflix.spinnaker.orca.pipeline.model
 
 import com.netflix.spinnaker.security.AuthenticatedRequest
 import org.slf4j.MDC
+import org.slf4j.helpers.NOPMDCAdapter
 import spock.lang.Specification
 import static com.netflix.spinnaker.orca.test.model.ExecutionBuilder.pipeline
 import static com.netflix.spinnaker.orca.test.model.ExecutionBuilder.stage
 
 class ExecutionSpec extends Specification {
+  void setupSpec() {
+    if (MDC.getMDCAdapter() instanceof NOPMDCAdapter) {
+      throw new IllegalStateException("ExecutionSpec.AuthenticationDetails tests cannot function " +
+              "without a real MDC implementation loaded by slf4j")
+    }
+  }
+
   def "should return Optional.empty if no authenticated details available"() {
     given:
     MDC.clear()